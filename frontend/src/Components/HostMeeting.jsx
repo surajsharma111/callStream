b7e61ebcd3bee4860188ec086de6bfb665eee85f
Suraj Sharma <125715507+surajsharma111@users.noreply.github.com>
2025-03-29 16:09:21 +1100

    edit node modules

--- frontend/src/Components/HostMeeting.jsx
+++ frontend/src/Components/HostMeeting.jsx
@@ -1,45 +1,48 @@
+import { CallingState, StreamCall, StreamVideo, StreamVideoClient, useCall, useCallStateHooks } from '@stream-io/video-react-sdk';
 
-import { FaVideo } from "react-icons/fa6";
 
 
 function HostMeeting() {
   
+  const apiKey = 'mmhfdzb5evj2';
+  const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL3Byb250by5nZXRzdHJlYW0uaW8iLCJzdWIiOiJ1c2VyL1BhZG1fX0FtaWRhbGEiLCJ1c2VyX2lkIjoiUGFkbV9fQW1pZGFsYSIsInZhbGlkaXR5X2luX3NlY29uZHMiOjYwNDgwMCwiaWF0IjoxNzQzMjIzMzUxLCJleHAiOjE3NDM4MjgxNTF9.duxna9f5NvQmQAlirQnFzRFPq1_wMHlS4tD6lHrMHwo';
+  const userId = 'Padm__Amidala';
+  const callId = 'WG91EONMjay7';
+
+  // set up the user object
+const user = {
+  id: userId,
+  name: 'Oliver',
+  image: 'https://getstream.io/random_svg/?id=oliver&name=Oliver',
+};
+const client = new StreamVideoClient({ apiKey, user, token });
+const call = client.call('default', callId);
+call.join({ create: true });
+ const MyUILayout = () => {
+  const call = useCall();
+
+  const { useCallCallingState, useParticipantCount } = useCallStateHooks();
+  const callingState = useCallCallingState();
+  const participantCount = useParticipantCount();
+
+  if (callingState !== CallingState.JOINED) {
+    return <div>Loading...</div>;
+  }
 
   return (
+    <div>
+      `    Call ${call.id} has {participantCount} participants`
+    </div>
+  );
+};
+  
+  return (
     
-        <div className="w-screen h-screen flex justify-center items-center bg-gray-100">
-          <div className="flex flex-col justify-center items-center w-full max-w-lg p-6 bg-white shadow-lg rounded-lg">
-            
-            {/* Logo & Title */}
-            <h1 className="flex flex-row items-center justify-center gap-2 font-bold text-blue-600 text-2xl">
-              CallStream <FaVideo />
-            </h1>
-            
-            <h2 className="w-full text-center font-bold text-blue-600 text-xl mt-4">
-              Host a Meeting
-            </h2>
-    
-            {/* Create Room Button */}
-            
-              <button
-                className="w-full px-4 py-3 mt-4 rounded-xl font-bold  bg-blue-500 hover:bg-blue-700 transition"
-                
-              >
-                Create Room
-              </button>
-             
-              <p className="text-gray-700 font-semibold mt-4">
-                Room ID: <span className="text-blue-500 font-bold"></span> (Share this ID with participants)
-              </p>
-            
-    
-            {/* Video Section */}
-            <div className="flex flex-col md:flex-row gap-4 mt-6">
-              <video  autoPlay playsInline muted className="w-40 h-28 border rounded-lg shadow-md" />
-              <video  autoPlay playsInline className="w-40 h-28 border rounded-lg shadow-md" />
-            </div>
-          </div>
-        </div>
+    <StreamVideo client={client}>
+    <StreamCall call={call}>
+      <MyUILayout />
+    </StreamCall>
+  </StreamVideo>
   )
 }
 export default HostMeeting;
