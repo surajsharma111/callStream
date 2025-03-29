275d03b3e84dcc68f3592a11f5c218cb993f896b
Suraj Sharma <125715507+surajsharma111@users.noreply.github.com>
2025-03-30 00:34:50 +1100

    Configured video chat

--- frontend/src/Components/HostMeeting.jsx
+++ frontend/src/Components/HostMeeting.jsx
@@ -1,48 +1,100 @@
-import { CallingState, StreamCall, StreamVideo, StreamVideoClient, useCall, useCallStateHooks } from '@stream-io/video-react-sdk';
+import {
+  CallControls,
+  CallingState,
+  SpeakerLayout,
+  StreamCall,
+  StreamTheme,
+  StreamVideo,
+  StreamVideoClient,
+  useCallStateHooks,
+} from "@stream-io/video-react-sdk";
 
+import { ParticipantView } from "@stream-io/video-react-sdk";
+import PropTypes from "prop-types";
 
+// ... rest of the App.tsx code
 
-function HostMeeting() {
-  
-  const apiKey = 'mmhfdzb5evj2';
-  const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL3Byb250by5nZXRzdHJlYW0uaW8iLCJzdWIiOiJ1c2VyL1BhZG1fX0FtaWRhbGEiLCJ1c2VyX2lkIjoiUGFkbV9fQW1pZGFsYSIsInZhbGlkaXR5X2luX3NlY29uZHMiOjYwNDgwMCwiaWF0IjoxNzQzMjIzMzUxLCJleHAiOjE3NDM4MjgxNTF9.duxna9f5NvQmQAlirQnFzRFPq1_wMHlS4tD6lHrMHwo';
-  const userId = 'Padm__Amidala';
-  const callId = 'WG91EONMjay7';
+const apiKey = "mmhfdzb5evj2";
+const token =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL3Byb250by5nZXRzdHJlYW0uaW8iLCJzdWIiOiJ1c2VyL1BhZG1fX0FtaWRhbGEiLCJ1c2VyX2lkIjoiUGFkbV9fQW1pZGFsYSIsInZhbGlkaXR5X2luX3NlY29uZHMiOjYwNDgwMCwiaWF0IjoxNzQzMjIzMzUxLCJleHAiOjE3NDM4MjgxNTF9.duxna9f5NvQmQAlirQnFzRFPq1_wMHlS4tD6lHrMHwo";
+const userId = "Padm__Amidala";
+const callId = "WG91EONMjay7";
 
-  // set up the user object
 const user = {
   id: userId,
-  name: 'Oliver',
-  image: 'https://getstream.io/random_svg/?id=oliver&name=Oliver',
+  name: "Suraj",
+  image: "https://getstream.io/random_svg/?id=oliver&name=Oliver",
 };
 const client = new StreamVideoClient({ apiKey, user, token });
-const call = client.call('default', callId);
+const call = client.call("default", callId);
 call.join({ create: true });
- const MyUILayout = () => {
-  const call = useCall();
 
-  const { useCallCallingState, useParticipantCount } = useCallStateHooks();
+const MyUILayout = () => {
+  const { useCallCallingState } = useCallStateHooks();
   const callingState = useCallCallingState();
-  const participantCount = useParticipantCount();
 
   if (callingState !== CallingState.JOINED) {
     return <div>Loading...</div>;
   }
 
   return (
-    <div>
-      `    Call ${call.id} has {participantCount} participants`
+    <StreamTheme>
+      <SpeakerLayout participantsBarPosition="bottom" />
+    
+        <CallControls />
+    
+    </StreamTheme>
+  );
+};
+
+function HostMeeting() {
+  return (
+    <StreamVideo client={client}>
+      <StreamCall call={call}>
+        <MyUILayout />
+      </StreamCall>
+    </StreamVideo>
+  );
+}
+export default HostMeeting;
+
+export const MyParticipantList = (props) => {
+  const { participants } = props;
+  return (
+    <div style={{ display: "flex", flexDirection: "row", gap: "8px" }}>
+      {participants.map((participant) => (
+        <ParticipantView
+          muteAudio
+          participant={participant}
+          key={participant.sessionId}
+        />
+      ))}
     </div>
   );
 };
-  
+
+MyParticipantList.propTypes = {
+  participants: PropTypes.arrayOf(PropTypes.object),
+};
+
+export const MyFloatingLocalParticipant = (props) => {
+  const { participant } = props;
   return (
-    
-    <StreamVideo client={client}>
-    <StreamCall call={call}>
-      <MyUILayout />
-    </StreamCall>
-  </StreamVideo>
-  )
-}
-export default HostMeeting;
+    <div
+      style={{
+        position: "absolute",
+        top: "15px",
+        left: "15px",
+        width: "240px",
+        height: "135px",
+        boxShadow: "rgba(0, 0, 0, 0.1) 0px 0px 10px 3px",
+        borderRadius: "12px",
+      }}
+    >
+      {participant && <ParticipantView muteAudio participant={participant} />}{" "}
+    </div>
+  );
+};
+MyFloatingLocalParticipant.propTypes = {
+  participant: PropTypes.object,
+};
